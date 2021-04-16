7492e2d29de093e74ef4e27ff680420c487a8425
amin <a.ahmadi@webmail.kian.digital>
2021-04-16 17:39:06 +0430

    Discovery

---
+++ microservice-common/src/main/java/com/packt/common/util/exceptions/EventProcessingException.java
@@ -0,0 +1,18 @@
+package com.packt.common.util.exceptions;
+
+public class EventProcessingException extends RuntimeException {
+    public EventProcessingException() {
+    }
+
+    public EventProcessingException(String message) {
+        super(message);
+    }
+
+    public EventProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EventProcessingException(Throwable cause) {
+        super(cause);
+    }
+}
