a2d75d75d3b90e5a610576f7865ef76c9aa1d4d8
amin <a.ahmadi@webmail.kian.digital>
2021-04-23 19:39:42 +0430

    config-server

---
+++ cloud-config-server/src/test/java/com/packt/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
